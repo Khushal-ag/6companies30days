0367b4a43b64a0b6663bf4cf64de7b657ee4e1a9
Khushal-ag <starkhush5@gmail.com>
2023-01-04 19:17:58 +0530

    q14

---
+++ Company 1 Microsoft/14. Leetcode 2344.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/minimum-deletions-to-make-array-divisible/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, vector<int> &numsDivide)
+    {
+        int gcd = numsDivide[0];
+        for (int i = 1; i < numsDivide.size(); i++)
+        {
+            gcd = __gcd(gcd, numsDivide[i]);
+        }
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+            if (gcd % nums[i] == 0)
+                return i;
+        return -1;
+    }
+};
