4010d137b16ca8020e3232e691bbd9145612244c
Khushal-ag <starkhush5@gmail.com>
2023-01-15 23:16:52 +0530

    q14

---
+++ Adobe/14. Leetcode 2327.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/number-of-people-aware-of-a-secret/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget)
+    {
+        vector<long> dp(n + 1);
+        dp[1] = 1;
+        int share = 0, mod = 1e9 + 7, res = 0;
+        for (int i = 2; i <= n; ++i)
+            dp[i] = share = (share + dp[max(i - delay, 0)] - dp[max(i - forget, 0)] + mod) % mod;
+        for (int i = n - forget + 1; i <= n; ++i)
+            res = (res + dp[i]) % mod;
+        return res;
+    }
+};
