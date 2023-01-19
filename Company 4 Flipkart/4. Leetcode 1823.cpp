2a8e1c9fd09ca914afd97dd357043ee2da61f40a
Khushal-ag <starkhush5@gmail.com>
2023-01-19 23:23:11 +0530

    q4

---
+++ Company 4 Flipkart/4. Leetcode 1823.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int helper(int n, int k)
+    {
+        if (n == 1)
+            return 0;
+        return (helper(n - 1, k) + k) % n;
+    }
+    int findTheWinner(int n, int k)
+    {
+        return helper(n, k) + 1; //+1 is for converting 0-based indexing to 1-based indexing
+    }
+};
