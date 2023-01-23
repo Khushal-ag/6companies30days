6893e19df5892c9e0e7889b0fbc850bc6b808fad
Khushal-ag <starkhush5@gmail.com>
2023-01-23 23:49:49 +0530

    q14

---
+++ Company 5 Google/14. Leetcode 2396.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/strictly-palindromic-number/
+#include<bits/stdc++.h>  
+using namespace std;
+
+class Solution
+{
+public:
+    bool isStrictlyPalindromic(int n)
+    {
+        return false;
+    }
+};
