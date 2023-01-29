9867b8b3fe323b6546a7010b561a606d9c2cab67
Khushal-ag <starkhush5@gmail.com>
2023-01-30 01:04:40 +0530

    q7

---
+++ Company 6 Amazon/7. Leetcode 649.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/dota2-senate/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string predictPartyVictory(string senate)
+    {
+        int count = 0, len = 0;
+        while (senate.size() != len)
+        {
+            string s;
+            len = senate.size();
+            for (int i = 0; i < len; i++)
+            {
+                if (senate[i] == 'R')
+                {
+                    if (count++ >= 0)
+                        s += 'R';
+                }
+                else if (senate[i] == 'D')
+                {
+                    if (count-- <= 0)
+                        s += 'D';
+                }
+            }
+            swap(s, senate);
+        }
+        if (senate[0] == 'R')
+            return "Radiant";
+        else
+            return "Dire";
+    }
+};
